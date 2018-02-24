243aa1abda605bb39173160c3aecd01716450c4e
blueyy <464176845@qq.com>
2018-02-24 19:45:10 +0800

    fix bug

--- resizeCanvas.js
+++ resizeCanvas.js
@@ -1,8 +1,10 @@
-function resizeCanvasToDisplaySize (canvas) {
+function resizeCanvasSize (gl) {
+	let canvas = gl.canvas
 	let width = canvas.clientWidth
 	let height = canvas.clientHeight
 	if (width !== canvas.width || height !== canvas.height) {
 		canvas.width = width
 		canvas.height = height
+		gl.viewport(0, 0, width, height)
 	}
 }
